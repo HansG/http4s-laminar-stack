80010a1308d375445bb7cb67a0b4ca7f7c1faa3b
HG <h.gail@web.de>
2023-09-14 15:01:43 +0200

    multipart basic HelloWorldServer

--- modules/backend/src/main/scala/trys/HelloWorldServer.scala
+++ modules/backend/src/main/scala/trys/HelloWorldServer.scala
@@ -11,28 +11,30 @@ import org.http4s.headers.*
 import org.http4s.multipart.{Multipart, Part}
 import fs2.{Pipe, Stream, text}
 import fs2.io.file.{Files, Path as FsPath}
+import org.http4s.implicits.http4sHeaderSyntax
 
 import scala.quoted.Type
 
 
 
 //https://stackoverflow.com/questions/47368919/processing-multipart-content-in-http4s
-object HelloWorldServer extends IOApp with Http4sDsl[IO] {
-  val service = HttpRoutes.of[IO] {
+object HelloWorldServer extends IOApp with Http4sDsl[IO] :
+  val service: HttpRoutes[IO] = HttpRoutes.of[IO] {
 
     case GET -> Root =>
       Ok(
         """|<html>
            |<body>
            |<form method="post" action="/post" enctype="multipart/form-data">
-           | <input type="date" name="birthDate" placeholder="birthDate">
-           | <input type="file" name="dataFile">
+           | <input type="date" name="birthDate" placeholder="birthDate"><p>
+           | <input type="file" name="dataFile"><p>
+           | <input type="file" name="dataFile1"><p>
            | <input type="submit">
            |</form>
         """.stripMargin)
         .map(resp => resp.withContentType(`Content-Type`(MediaType.text.html)))
 
- /*   case req@POST -> Root / "post" =>
+    /*   case req@POST -> Root / "post" =>
       println(s"POST request: $req")
       req.decode[Multipart[IO]] { m =>
         Ok(
@@ -47,18 +49,19 @@ object HelloWorldServer extends IOApp with Http4sDsl[IO] {
 
     case req@POST -> Root / "post" =>
       req.decode[Multipart[IO]] { m =>
-        val dataParts = m.parts.filter(_.name == Some("dataFile"))
-        Traverse[Vector].traverse(dataParts) { part  =>
-           val lineStream = Stream(part.name.get, part.headers.mkString("\nheaders: ", ", ", "\n", _ => true)).through(text.utf8.encode) ++
-             part.body
+        val dataParts = m.parts.filter( pio =>  pio.name.exists(_.contains("dataFile")) )
+        Traverse[Vector].traverse(dataParts) { part =>
+          val lineStream = Stream(part.name.get, part.headers.mkString("\nheaders: ", ", ", "\n", _ => true)).through(text.utf8.encode) ++
+            part.body
           val sink: Pipe[IO, Byte, Unit] = Files[IO].writeAll(FsPath(part.name.get))
           lineStream.through(sink).compile.drain
-        }.flatMap( _ => Ok(s"""Multipart File-Data of ${dataParts.length}Parts:))""") )
+        }.flatMap(_ => Ok(s"""Multipart File-Data of ${dataParts.length}Parts:))"""))
       }
+  }
 
 
 
-  def resource(args: List[String]) =//, requestShutdown: IO[Unit]) =
+  def resource(args: List[String]) =  //, requestShutdown: IO[Unit]) =
     EmberServerBuilder
       .default[IO]
       .withPort(Port.fromInt(8080).get)
@@ -69,8 +72,7 @@ object HelloWorldServer extends IOApp with Http4sDsl[IO] {
 
   def run(args: List[String]): IO[ExitCode] =
         resource(args)
-          .use(_ => status *> IO.never)
+          .useForever  //(_ =>  IO.never)  //status *>
           .as(ExitCode.Success)
 
 
-}
